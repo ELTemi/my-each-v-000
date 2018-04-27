491b93e004638f7ddf5aad3b6a3d2fffbaa7d7af
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 18:42:54 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,12 @@
 def my_each(array)
   i = 0
   while i < array.length
-    my_each(array) do |word|
-      puts word
-    end
     yield array[i]
     i += 1
   end
   array
+end
+
+my_each(array) do |word|
+  puts word
 end
