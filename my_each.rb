ddf76546e8c8d2fc4d79f31397a1b80223561b72
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 18:29:06 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
-def my_each(mine) # put argument(s) here
+def my_each(array) # put argument(s) here
   if block.given?
     i = 0
     while i < mine.length
       yield mine[i]
       i += 1
     end
-    mine
+    array
   end# code here
 end
 
-my_each(mine) { |i| puts i}
+my_each(array) { |i| puts i}
