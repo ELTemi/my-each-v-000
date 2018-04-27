6b9755be194eaa588c17dbc031b8a980fa83745d
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 18:39:02 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,3 @@
-def my_each(array) # put argument(s) here
-  i = 0
-  while i < mine.length
-    yield mine[i]
-    i += 1
-  end
-  array
-end
-
 def my_each(array)
   my_each(array) do |word|
     i = 0
